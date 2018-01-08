3aac609acd9f475bd8c39e5398dee7b7ed9074fb
ZakaryWu <493336530@qq.com>
2018-01-08 13:57:35 +0800

    修改屏幕分辨率为1024*600

--- gui/comm/au_other_winres.h
+++ gui/comm/au_other_winres.h
@@ -37,8 +37,11 @@ extern "C" {
 #define SKIN_4				4						// 皮肤方案4    
 
 #ifdef _TFT_7_
-#define SCREEN_WIDTH	  		800						// 窗体的宽度
-#define SCREEN_HEIGHT     		480						// 窗体的高度
+#define SCREEN_WIDTH	  		1024				// 窗体的宽度
+#define SCREEN_HEIGHT     		600					// 窗体的高度
+#define LCD_W					SCREEN_WIDTH
+#define LCD_H					SCREEN_HEIGHT
+
 #define DW_STYLE				(WS_VISIBLE | WS_NONE)	// 窗体的风格
 //#define DW_STYLE_EX			WS_EX_NONE				// 窗体的扩展风格
 #define DW_STYLE_EX				WS_EX_AUTOSECONDARYDC	// 窗体的扩展风格
@@ -46,8 +49,11 @@ extern "C" {
 #define FORM_Y					0						// 起点坐标y 
 #define FORM_W					(SCREEN_WIDTH-FORM_X)	// 窗体的宽度
 #define FORM_H					(SCREEN_HEIGHT-FORM_Y)	// 窗体的高度
-#define FORM_RIGHT_W			160						// 右边窗体宽度
-#define FORM_RIGHT_H			480						// 右边窗体高度
+#define FORM_RIGHT_W			204						// 右边窗体宽度
+#define FORM_RIGHT_H			LCD_H					// 右边窗体高度
+#define FORM_LEFT_W				(LCD_W - FORM_RIGHT_W)	// 左边窗体宽度
+#define FORM_LEFT_H				LCD_H					// 左边窗体高度
+
 
 //#define DEFAULT_STYLE	  		(WS_VISIBLE | WS_NONE)	// 默认窗体的风格
 //#define DEFAULT_STYLE_EX  	(WS_EX_NONE)			// 默认窗体的扩展风格
@@ -77,7 +83,7 @@ extern "C" {
 #define KEYBOARD_CTRL_Y         (FORM_H-KEYBOARD_CTRL_H)
 
 // 右边控件宏定义
-#define RIGHT_CTRL_X			640
+#define RIGHT_CTRL_X			FORM_LEFT_W
 #define RIGHT_CTRL_Y			0
 #define RIGHT_CTRL_W			160
 #define RIGHT_CTRL_H			480
@@ -104,8 +110,11 @@ extern "C" {
 #define VOLUME_TOTAL_W			(210+48+20)
 #define VOLUME_ALL_ICON_Y		((VIDEO_WIN_H-VOLUME_TOTAL_H)/2+150)
 #else
-#define SCREEN_WIDTH	  		480						// 窗体的宽度
-#define SCREEN_HEIGHT     		272						// 窗体的高度
+#define SCREEN_WIDTH	  		1024				// 窗体的宽度
+#define SCREEN_HEIGHT     		600					// 窗体的高度
+#define LCD_W					SCREEN_WIDTH
+#define LCD_H					SCREEN_HEIGHT
+
 #define DW_STYLE				(WS_VISIBLE | WS_NONE)	// 窗体的风格
 //#define DW_STYLE_EX			WS_EX_NONE				// 窗体的扩展风格
 #define DW_STYLE_EX				WS_EX_AUTOSECONDARYDC	// 窗体的扩展风格
